699534af512d79ac9eb619e26f3c6f58b168ca17
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-01-26 13:23:31 +0100

    prt creados

--- src/biblio/Menu.java
+++ src/biblio/Menu.java
@@ -1,10 +1,24 @@
 package biblio;
 
 public class Menu {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void mostrarMenuPrincipal() {
+		System.out.println("Menu principal");
+		System.out.println("1. Gestionar Libros");
+		System.out.println("2. Gestionar Socios");
+		System.out.println("3. Gestionar Prestamos");
 	}
-
+	public static void mostrarMenuLibros() {
+		System.out.println("Menu libros");
+		System.out.println("1. Insertar libro");
+		System.out.println("2. Eliminar libro");
+		System.out.println("3. Ver libros");
+	}
+	public static void mostrarMenuSocios() {
+		System.out.println("Menu socios");
+		System.out.println("1. Ver socios");
+	}
+	public static void mostrarMenuPrestamos() {
+		System.out.println("Menu prestamos");
+		System.out.println("1. Realizar prestamo");
+	}
 }
