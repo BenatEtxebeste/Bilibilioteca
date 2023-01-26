32d303e873cf636dce86062e432ed602421717c9
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-01-26 13:23:19 +0100

    Connection and setters/getters added

---
+++ src/biblio/Conector.java
@@ -0,0 +1,35 @@
+package biblio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+	protected Connection con;
+	
+	public void conectar() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrar() throws SQLException {
+		con.close();
+	}
+	
+	public Connection getCon() {
+		return con;
+	}
+	
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+	
+}
