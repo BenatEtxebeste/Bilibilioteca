f6626ef44f842f54d9cb1747c3bc5356c35f2bc2
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-01-30 13:16:02 +0100

    corregido

--- src/biblio/Prestamo.java
+++ src/biblio/Prestamo.java
@@ -3,15 +3,35 @@ package biblio;
 import java.sql.Date;
 
 public class Prestamo {
+	private int id_libro;
+	private int id_socio;
 	private Date fecha;
 	private boolean devuelto;
 	
 	public Prestamo() {
 	}
 
-	public Prestamo(Date fecha, boolean devuelto) {
+	public Prestamo(int id_libro, int id_socio, Date fecha, boolean devuelto) {
+		this.id_libro = id_libro;
+		this.id_socio = id_socio;
 		this.fecha = fecha;
 		this.devuelto = devuelto;
+	}
+
+	public int getId_libro() {
+		return id_libro;
+	}
+
+	public void setId_libro(int id_libro) {
+		this.id_libro = id_libro;
+	}
+
+	public int getId_socio() {
+		return id_socio;
+	}
+
+	public void setId_socio(int id_socio) {
+		this.id_socio = id_socio;
 	}
 
 	public Date getFecha() {
