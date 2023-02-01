2b10274c9d651a919bbb61f3c7f70fb299e8f63d
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-01 13:55:41 +0100

    gestor libro terminado

--- src/biblio/GestorLibros.java
+++ src/biblio/GestorLibros.java
@@ -1,10 +1,15 @@
 package biblio;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GestorLibros {
 	
-	public static void run(Scanner scan) {
+	public static void run(Scanner scan) throws SQLException {
 		
 		int opcion;
+		Libro libro = new Libro();
 		GestorBBDD gestorBBDD = new GestorBBDD();
 		do {
 			Menu.mostrarMenuLibros();
@@ -12,8 +17,33 @@ public class GestorLibros {
 			
 			switch (opcion) {
 			case Menu.INSERTAR_LIBRO:
-				System.out.println("ins libro");
+				
+				libro = FormularioDeDatos.pedirDatosLibro(scan);
+				gestorBBDD.insertarLibro(libro);
+				
+				break;
+			case Menu.ELIMINAR_LIBRO:
+				
+				gestorBBDD.eliminarLibro(FormularioDeDatos.pedirIdLibro(scan));
+				
+				break;
+			case Menu.MODIFICAR_LIBRO:
+				
+				libro = gestorBBDD.getLibro(FormularioDeDatos.pedirIdLibro(scan));
+				FormularioDeDatos.modificarDatosLibro(libro, scan);
+				gestorBBDD.modificarLibro(libro);
+				
+				break;
+			case Menu.VER_LIBROS:
+				
+				ArrayList<Libro> libros = new ArrayList<Libro>();
+				Visor.mostrarLibros(libros);
+				
+				break;
+			case Menu.SALIR:
+				System.out.println("Volviendo al gestor de biblioteca");
+				break;
 			}
-		}while (opcion != 5);
+		}while (opcion != Menu.SALIR);
 	}
 }
