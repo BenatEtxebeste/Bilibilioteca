5ff5267ae47baf441bf22253338b7ce0e088dc2d
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-02-02 11:44:57 +0100

    gestorBBDD terminado

--- src/biblio/GestorBBDD.java
+++ src/biblio/GestorBBDD.java
@@ -45,7 +45,12 @@ public class GestorBBDD extends Conector{
 		String sentenciaSelect = "SELECT * FROM libros";
 		ResultSet resultado = pst.executeQuery(sentenciaSelect);
 		while (resultado.next()) {
-			System.out.println(resultado.getInt(1) + ". " + resultado.getString(2));
+			Libro libro = new Libro();
+			libro.setId(resultado.getInt("id"));
+			libro.setTitulo(resultado.getString("titulo"));
+			libro.setAutor(resultado.getString("autor"));
+			libro.setNum_pag(resultado.getInt("num_pag"));
+			libros.add(libro);
 		}
 		return libros;
 	}
@@ -61,4 +66,72 @@ public class GestorBBDD extends Conector{
 		pst.execute();
 	}
 	
+	public void insertarSocio(Socio socio) throws SQLException {
+		super.conectar();
+		pst = con.prepareStatement("INSERT INTO socios VALUES (null, ?, ?, ?, ?, ?, ?)");
+		pst.setString(1, socio.getNombre());
+		pst.setString(2, socio.getApellido());
+		pst.setString(3, socio.getDireccion());
+		pst.setString(4, socio.getPoblacion());
+		pst.setString(5, socio.getProvincia());
+		pst.setString(6, socio.getDni());
+		
+		pst.execute();
+		super.cerrar();
+	}
+	
+	public void eliminarSocio(int id) throws SQLException {
+		super.conectar();
+		pst = con.prepareStatement("DELETE FROM socios WHERE id = ?");
+		pst.setInt(1, id);
+	}
+	
+	public Socio getSocio(int id) throws SQLException {
+		super.conectar();
+		String sentenciaSelect = "SELECT * FROM socios WHERE id = ?";
+		pst.setInt(1, id);
+		Socio socio = new Socio();
+		ResultSet resultado = pst.executeQuery(sentenciaSelect);
+		socio.setId(resultado.getInt("id"));
+		socio.setNombre(resultado.getString("nombre"));
+		socio.setApellido(resultado.getString("apellido"));
+		socio.setDireccion(resultado.getString("direccion"));
+		socio.setPoblacion(resultado.getString("poblacion"));
+		socio.setProvincia(resultado.getString("provincia"));
+		socio.setDni(resultado.getString("dni"));
+		
+		return socio;
+	}
+	
+	public ArrayList<Socio> getSocios() throws SQLException {
+		super.conectar();
+		ArrayList<Socio> socios = new ArrayList<Socio>();
+		String sentenciaSelect = "SELECT * FROM libros";
+		ResultSet resultado = pst.executeQuery(sentenciaSelect);
+		while (resultado.next()) {
+			Socio socio = new Socio();
+			socio.setNombre(resultado.getString("nombre"));
+			socio.setApellido(resultado.getString("apellido"));
+			socio.setDireccion(resultado.getString("direccion"));
+			socio.setPoblacion(resultado.getString("poblacion"));
+			socio.setProvincia(resultado.getString("provincia"));
+			socio.setDni(resultado.getString("dni"));
+			socios.add(socio);
+		}
+		return socios;
+	}
+	
+	public void modificarSocio(Socio socio) throws SQLException {
+		super.conectar();
+		pst = con.prepareStatement("UPDATE socios set nombre = ?, apellido = ?, direccion = ?, poblacion = ?, provincia = ?, dni = ? WHERE id = ?");
+		pst.setString(1, socio.getNombre());
+		pst.setString(2, socio.getApellido());
+		pst.setString(3, socio.getDireccion());
+		pst.setString(4, socio.getPoblacion());
+		pst.setString(5, socio.getProvincia());
+		pst.setString(6, socio.getDni());
+		pst.setInt(7, socio.getId());
+		
+		pst.execute();
+	}
 }
